c9dc5809dafe4332b2f4654fe836414b44643724
petersercombe <peter@petersercombe.com>
2016-12-09 16:56:20 +1000

    WIP: Existing plugins complete. New plugins coming

--- installplugins.sh
+++ installplugins.sh
@@ -69,10 +69,18 @@ git clone -b master https://github.com/davosmith/moodle-grade_checklist.git chec
 
 
 # Atto Plugins
+cd $moodleDirectory/html/lib/editor/atto/plugins
 
+git clone -b master https://github.com/dthies/moodle-atto_fullscreen.git fullscreen
+git clone -b master https://github.com/moodleuulm/moodle-atto_styles.git styles
+git clone -b master https://github.com/cdsmith-umn/pastespecial.git pastespecial
+git clone -b master https://github.com/dthies/moodle-atto_cloze.git cloze
+git clone -b master https://github.com/damyon/moodle-atto_count.git count
 
 # Enrolment Methods
+cd $moodleDirectory/html/enrol
 
+git clone -b master https://github.com/markward/enrol_autoenrol.git autoenrol
 
 # Availability Restrictions
 cd $moodleDirectory/html/availability/condition
@@ -81,15 +89,26 @@ git clone -b master https://github.com/moodlehq/moodle-availability_mobileapp.gi
 git clone -b master https://github.com/FMCorz/moodle-availability_xp.git xp
 
 # Course Formats
+cd cd $moodleDirectory/html/course/format
 
+git clone -b MOODLE_31 --single-branch https://github.com/gjb2048/moodle-format_topcoll.git topcoll
+git clone -b MOODLE_31 --single-branch https://github.com/gjb2048/moodle-format_grid.git grid
 
 # Themes
+cd cd $moodleDirectory/html/theme
 
+git clone -b master https://github.com/gjb2048/moodle-theme_essential.git essential
 
 # Local Plugins
 cd $moodleDirectory/html/local
 
 git clone -b $moodleVersion --single-branch https://github.com/moodlehq/moodle-local_mobile.git mobile
+git clone -b master https://github.com/markward/local_autogroup.git autogroup
+
+# Admin Tools
+cd $moodleDirectory/admin/tool
+
+git clone -b master https://github.com/moodlehq/moodle-tool_lpimportcsv lpimportcsv
 
 # Alternative Login Form
 cd $moodleDirectory/html
